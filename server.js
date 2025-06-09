77c414a3e37f1de84619b4d598b752aa71b5a521
johnycampos <johny.campos@ymail.com>
2025-06-09 00:46:49 -0300

    ajustes vercel

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Configuração do CORS
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://coworking-navy.vercel.app/'],
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
@@ -299,7 +294,13 @@ app.get('/api/health', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// Exporta a aplicação para o Vercel
+module.exports = app;
+
+// Inicia o servidor apenas se não estiver em ambiente serverless
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
